48b36201545218aaf524f52ede0c90806529a256
jungleehyun <nickhyun@jungleehyuns-MacBook-Air.local>
2022-07-06 00:59:31 +0900

    fix hazelcast but failed

--- dockerHazelcast/2startMancenter.sh
+++ dockerHazelcast/2startMancenter.sh
@@ -3,12 +3,13 @@ MC_VERSION=5.1.3
 
 docker run -d \
  --name $PS_NAME \
+ --env MC_HTTP_PORT=38080 \
  --env MC_CONTEXT_PATH=/mc \
  --env MC_ADMIN_USER=admin \
  --env MC_ADMIN_PASSWORD=myPassword11 \
  --env MC_DEFAULT_CLUSTER="dev" \
- --env MC_DEFAULT_CLUSTER_MEMBERS="127.0.0.1" \
+ --env MC_DEFAULT_CLUSTER_MEMBERS="192.168.29.187" \
  -v PATH_TO_PERSISTENT_FOLDER:/data \
- --rm -p 38080:8080 \
+ -p 38080:38080 \
 hazelcast/management-center:$MC_VERSION
 
--- dockerHazelcast/4stopMancenter.sh
+++ dockerHazelcast/4stopMancenter.sh
@@ -1,1 +1,5 @@
+
+word1=$(docker ps -a | grep mancenter | awk 'BEGIN { FS="[ ]" } ; { print $1 }')
+
 docker stop mancenter
+docker rm $word1
